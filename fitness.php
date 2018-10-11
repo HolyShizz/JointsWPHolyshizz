82a55c6d69acf7e3eef9167a7ebddd98db0ccf7a
Pylyp <filippsoltys@gmail.com>
2018-10-12 02:31:05 +0300

    media query max-wid 1024

--- fitness.php
+++ fitness.php
@@ -11,12 +11,13 @@ get_header(); ?>
 
 		<main class="main small-12 medium-12 large-12 cell" role="main">
 			<div class="block-quote">
-				<div class="grid-container fluid">
+				<div class="grid-container ">
 					<div class="grid-x align-center">
-						<div class="cell small-2 medium-2 large-1"><img src="<?php echo ot_get_option('image_inside_block_quote'); ?>"
-							 alt=""></div>
-						<div class="cell small-10 medium-8 large-4">
+						<div class="cell large-8 ">
+							<div class="quote-container"><img src="<?php echo ot_get_option('image_inside_block_quote'); ?>"
+							 alt="">
 							<?php echo ot_get_option('text_inside_block_quote'); ?>
+						</div>
 						</div>
 					</div>
 				</div>
@@ -57,7 +58,7 @@ get_header(); ?>
 						?>
 						</h4>
 					</div>
-					<div class="grid-x align-justify grid-margin-x">
+					<div class="grid-x align-justify align-center grid-padding-x">
 
 						<?php 
 							$advantages_item = ot_get_option('card_advantages_1_3', array() );
@@ -65,7 +66,7 @@ get_header(); ?>
 								if ( !empty ($advantages_item)) {
 								
 								foreach ($advantages_item as $advantages_item_single) {
-									echo  '<div class="cell large-3 medium-8 small-12">
+									echo  '<div class="cell large-4 medium-8 small-12">
 										<h1>'.$advantages_item_single['card_advantages_1_3_item_number'].'</h1>
 										<h4>'.$advantages_item_single['title'].'</h4>
 										<p>'.$advantages_item_single['card_advantages_item_1_3_paragraph'].'</p>
@@ -126,7 +127,7 @@ get_header(); ?>
 						?>
 						</h4>
 					</div>
-					<div class="grid-x align-justify padding-x">
+					<div class="grid-x align-justify grid-padding-x">
 
 						<?php 
 							$advantages_items = ot_get_option('card_advantages_4_6', array() );
@@ -134,7 +135,7 @@ get_header(); ?>
 								if ( !empty ($advantages_items)) {
 								
 								foreach ($advantages_items as $advantages_item_single) {
-									echo  '<div class="cell large-3 medium-4 small-12">
+									echo  '<div class="cell large-4 medium-8 small-12">
 										<h1>'.$advantages_item_single['card_advantages_4_6_number'].'</h1>
 										<h4>'.$advantages_item_single['title'].'</h4>
 										<p>'.$advantages_item_single['card_advantages_4_5_paragraph'].'</p>
@@ -150,6 +151,140 @@ get_header(); ?>
 			<div class="parallax" style="background-image:url(<?php echo ot_get_option('parallax_images_1')?>);">
 
 			</div>
+
+			<div class="block-swiper">
+				<div class="coursec" id="coursec">
+					<div class="row">
+						<div class="column">
+							<!--Courses 1 step-->
+							<h3>
+								<?php echo ot_get_option('coursec_title')?>
+							</h3>
+
+							<?php 
+								$coursec_nav = ot_get_option('upper_tabs_nav_and_content', array());
+								if (!empty ($coursec_nav) ) {
+									echo '<div class="button-group expanded">';
+									foreach ($coursec_nav as $coursec_nav_single) {
+									echo	'<button class="button secondary">'.$coursec_nav_single['title'].'</button>';
+									}
+									echo '	 </div>';
+									
+									
+								}
+								?>
+							<!--Courses 2 step-->
+							<h3>
+								<?php echo ot_get_option('coursec_step_2_title')?>
+							</h3>
+
+							<?php 
+								$coursec_step2 = ot_get_option('coursec_step_2_card', array());
+								if (!empty ($coursec_step2)) {
+									echo '<div class="grid-container">
+											  <div class="grid-x grid-padding-x medium-up-4">';
+									foreach ($coursec_step2 as $coursec_step2_single) {
+									echo '<div class="cell">
+											<div class="card text-center">
+												<div class="card-section">
+													<h5 class="course-name">'.$coursec_step2_single['title'].'</h5>
+													<h4>'.$coursec_step2_single['coursec_step_2_card_price_title'].'</h4>
+													<h4>'.$coursec_step2_single['coursec_step_2_card_price_number'].'</h5>
+													<button class="button primary" data-open="'.$coursec_step2_single['js_buttondata_2nd_section'].'">
+													Подробности и оплата
+													</button>
+													
+												</div>
+											</div>
+
+											<div class="grid-container large reveal" id="'.$coursec_step2_single['js_buttondata_2nd_section'].'" data-reveal>
+													<div class="grid-x grid-padding-x ">
+															<button class="close-button" data-close aria-label="Close modal" type="button">
+																	<span aria-hidden="true">&times;</span>
+																  </button>
+														<div class="cell medium-6 ">
+															<h5>'.$coursec_step2_single['title'].'</h5>
+															<p>'.$coursec_step2_single['coursec_step_2_card_included_text'].'</p>
+															<p>'.$coursec_step2_single['coursec_step_2_card_purpose'].'</p>
+														</div>
+
+														<div class="cell medium-6 ">
+																<h5>'.$coursec_step2_single['coursec_step_2_card_included_title'].'</h5>
+																<p>'.$coursec_step2_single['coursec_step_2_card_included_text'].'</p>
+																<h5>'.$coursec_step2_single['coursec_step_2_card_price_title'].'</h5>
+																<h3>'.$coursec_step2_single['coursec_step_2_card_price_number'].'</h3>
+														</div>
+
+													</div>
+												</div> 
+										  </div> 
+										  
+										  ';
+									}
+
+											  
+									echo '	   </div>
+										  </div>';	  
+								}
+								?>
+						</div>
+						<!--Courses 3 step-->
+						<h3>
+								<?php echo ot_get_option('coursec_step_3_title')?>
+							</h3>
+						<?php 
+								$coursec_step3 = ot_get_option('coursec_step_3_card', array());
+								if (!empty ($coursec_step3)) {
+									echo '<div class="grid-container">
+											  <div class="grid-x grid-padding-x medium-up-4">';
+									foreach ($coursec_step3 as $coursec_step3_single) {
+									echo '<div class="cell">
+											<div class="card text-center">
+												<div class="card-section">
+													<h5 class="course-name">'.$coursec_step3_single['title'].'</h5>
+													<h4>'.$coursec_step3_single['coursec_step_3_card_price_title'].'</h4>
+													<h4>'.$coursec_step3_single['coursec_step_3_card_price_number'].'</h5>
+													<button class="button primary" data-open="'.$coursec_step3_single['js_buttondata_3nd_section'].'">
+													Подробности и оплата
+													</button>
+													
+												</div>
+											</div>
+
+											<div class="grid-container large reveal" id="'.$coursec_step3_single['js_buttondata_3nd_section'].'" data-reveal>
+													<div class="grid-x grid-padding-x ">
+															<button class="close-button" data-close aria-label="Close modal" type="button">
+																	<span aria-hidden="true">&times;</span>
+																  </button>
+														<div class="cell medium-6 ">
+															<h5>'.$coursec_step3_single['title'].'</h5>
+															<p>'.$coursec_step3_single['coursec_step_3_card_included_text'].'</p>
+															<p>'.$coursec_step3_single['coursec_step_3_card_purpose'].'</p>
+														</div>
+
+														<div class="cell medium-6 ">
+																<h5>'.$coursec_step3_single['coursec_step_3_card_included_title'].'</h5>
+																<p>'.$coursec_step3_single['coursec_step_3_card_included_text'].'</p>
+																<h5>'.$coursec_step3_single['coursec_step_3_card_price_title'].'</h5>
+																<h3>'.$coursec_step3_single['coursec_step_3_card_price_number'].'</h3>
+														</div>
+
+													</div>
+												</div> 
+										  </div> 
+										  
+										  ';
+									}
+
+											  
+									echo '	   </div>
+										  </div>';	  
+								}
+								?>
+					</div>
+				</div>
+			</div>
+
 
 			<div class="testimonials grid-container" id="testimonials" data-magellan-target="testimonials">
 				<?php 
@@ -187,7 +322,7 @@ get_header(); ?>
 								</div>';
 								?>
 
-								<?php 
+				<?php 
 									echo '<div class="results">
 											<div class="row">
 												<div class="columns">
